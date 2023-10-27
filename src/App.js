5464905508a6d59bbb8900148ab5caab546a2ae6
Muskan Pandey <Muskanpandey1003@gmail.com>
2023-10-27 15:14:50 +0530

    blog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from "react";
+import Header from "./components/Header";
+import Blog from "./components/Blog";
+import Pagination from "./components/Pagination";
+import { AppContext } from "./context/AppContext";
+
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const {fetchBlogPosts} = useContext(AppContext);
+  
+  useEffect(()=>{
+    fetchBlogPosts();
+  },[])
+
+  return(
+    <div className="w-full h-full flex flex-col gap-y-1  ">
+    
+      <Header />
+      <Blog />
+      <Pagination />
+   
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='border shadow-md py-2'>
+        <header className='text-center'>
+       < h1 className='text-xl font-bold uppercase '>The Coding Canvas Blogs</h1>
+        </header>
+      
+    </div>
+  )
+}
+
+export default Header
